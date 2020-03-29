d65d11fc1955f8a136a7e785c464431ae0dab8c6
ecrmnn <danieleckermann@gmail.com>
2020-03-29 17:56:32 +0200

    Fixes #5

--- image/providers/PlaceholdIt.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  getImageUrl: function (size) {
-    'use strict';
-
-    var pieces = size.split('x');
-
-    return 'http://placeholdit.imgix.net/~text?txtsize=40&txt=Spaceholder&w=' + pieces[0] + '&h=' + pieces[1];
-  }
-};
